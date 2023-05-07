5ff9f3d03f928bde47d0fa79b465e0668195c09d
Michael Mason <michaelmason@live.com.au>
2023-05-07 09:43:27 -0400

    add tests

--- src/TrieTreeBuilderTest.java
+++ src/TrieTreeBuilderTest.java
@@ -10,14 +10,20 @@ class TrieTreeBuilderTest {
         ttb.setUp("test/basic");
         Node root = ttb.buildTree();
         System.out.println(root);
+        assertNull(root.getSpecName());
+        assertNull(root.getLeftChild().getSpecName());
+        assertEquals("Test2 test2", root.getLeftChild().getLeftChild().getSpecName());
     }
 
     @org.junit.jupiter.api.Test
     void testBuildTreeComplex() {
         SequenceParser sp = new SequenceParser(Integer.MAX_VALUE);
-        List<Species> specList = sp.parseFolder("sequences");
+        List<Species> specList = sp.parseFolder("test/complex");
         TrieTreeBuilder ttb = new TrieTreeBuilder(specList);
         Node root = ttb.buildTree();
         System.out.println(root);
+        assertNull(root.getSpecName());
+        assertNull(root.getLeftChild().getSpecName());
+        assertEquals("Anas platyrhynchos", root.getLeftChild().getLeftChild().getSpecName());
     }
 }
